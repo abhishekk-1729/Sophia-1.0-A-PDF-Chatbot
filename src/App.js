13a3a87f147cdb1b2ba13d905c5a2664e77cea70
abhishekk-1729 <135116930+abhishekk-1729@users.noreply.github.com>
2023-12-21 21:31:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './App.scss'
+import { useState } from 'react';
+import CentreText from './Components/CentreText';
+import FileList from './Components/FileList';
+import Uploader from './Components/Uploader';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
 
 function App() {
+
+
+  const [files,setFiles] = useState([])
+  const [pdfText,setPdfText] = useState("")
+
+  const removeFile = (filename) => {
+    setFiles([])
+  }
+
+
+
+
   return (
+   
+    <>
+    <NavBar/>
+    <CentreText/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="title">
+            Upload file
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Uploader files={files} setFiles={setFiles} removeFile={removeFile} pdfText={pdfText} setPdfText={setPdfText} />
+
+        <FileList files={files} removeFile={removeFile} pdfText={pdfText}/>
+
     </div>
+
+    
+
+
+
+    {/* <Footer/> */}
+    </>
   );
 }
 
